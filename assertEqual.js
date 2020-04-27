98777cfb326e6f08d0e52ca9bc816fd554b3670f
Ben S <shaw.j.benjamin@gmail.com>
2020-04-27 23:18:08 +0000

    Lotide assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔺🔺🔺 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual(4,9);
