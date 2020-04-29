0a03e946f6cc721ce82cd398fc5554bbf1148785
Ben S <shaw.j.benjamin@gmail.com>
2020-04-29 00:03:18 +0000

    added assertEqual and eqArrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🔺🔺🔺 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔺🔺🔺 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
