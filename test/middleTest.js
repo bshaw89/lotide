59d6de27bb090622470579c3701857d63edd5696
Ben S <shaw.j.benjamin@gmail.com>
2020-05-04 20:53:17 +0000

    refactored middleTest.js and tailTest.js to use chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,18 +1,26 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
- let arr1 = [1,2];
- let arr2 = [1,2,3];
- let arr3 = [1,2,4,6,9];
- let arr4 = [2,2,3,4,5,6];
+let arr1 = [1,2];
+let arr2 = [1,2,3];
+let arr3 = [1,2,4,6,9];
+let arr4 = [2,2,3,4,5,6];
 
- console.log("Sending in first array, expecting true");
- assertArraysEqual(middle(arr1), []);
- console.log("Sending in second array, expecting not true");
- assertArraysEqual(middle(arr2), [1,2,3]);
- console.log("Sending in first array, expecting not true");
- assertArraysEqual(middle(arr1), [1,2,3]);
- console.log("Sending in third array, expecting true");
- assertArraysEqual(middle(arr3), [4]);
- console.log("Sending in fourth array, expecting true");
- assertArraysEqual(middle(arr4), [3,4]);
+describe("#middle", () => {
+  it("returns true", () => {
+    assert.deepEqual(middle(arr1), []);
+  });
+  it("returns not true", () => {
+    assert.deepEqual(middle(arr2), [2]);
+  });
+  it("returns not true", () => {
+    assert.notDeepEqual(middle(arr1), [1,2]);
+  });
+  it("returns true", () => {
+    assert.deepEqual(middle(arr3), [4]);
+  });
+  it("returns true", () => {
+    assert.deepEqual(middle(arr4), [3,4]);
+  });
+});
