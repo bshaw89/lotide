adf912ad75bbccce53f8296c1b94428f906a8b94
Ben S <shaw.j.benjamin@gmail.com>
2020-05-04 18:49:34 +0000

    refactored assertArraysEqual.js tests into separate file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,17 @@
-const eqArrays = function(arr1, arr2) {
- /* if (arr1.length !== arr2.length) {
-    return false;
-  } */
-  for (let i = 0; i < arr1.length; i++) {
-   // console.log(arr1[i]);
-   if (arr1[i] !== arr2[i]) {
-    return false;
-   }
-  }
-  return true;
-}
-
-// 
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
- // console.log(eqArrays());
+  // console.log(eqArrays());
+ 
+  if(eqArrays(arr1, arr2)) {
+    //display some positive message affirming the match
+    console.log("True 🟢");
+  } else {
+    //the opposite
+     console.log("Not true 🔴");
+   }
+ }
 
- if(eqArrays(arr1, arr2)) {
-   //display some positive message affirming the match
-   console.log("It's true.");
- } else {
-   //the opposite
-    console.log("It's not true.");
-  }
-}
 
-assertArraysEqual([1,2,3,4], [1,2,3])
+
+module.exports = assertArraysEqual;
